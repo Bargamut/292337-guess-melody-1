c82ea64c3b57cd4108b9ff586f2e10af385b3b95
Paul Petrov <paul@bargamut.ru>
2019-05-06 03:33:16 +0300

    Написать E2E-тест компонента Экрана приветствия
    * E2E-тест отработки клика по кнопке старта

---
+++ src/components/welcome-screen/welcome-screen.e2e.test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Enzyme, {shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import WelcomeScreen from './welcome-screen.jsx';
+
+Enzyme.configure({adapter: new Adapter()});
+
+it(`Start button click callback runs`, () => {
+  const clickHandler = jest.fn();
+
+  const welcomeScreen = shallow(
+      <WelcomeScreen
+        time={0}
+        errorCount={0}
+        onClickStartBtn={clickHandler}
+      />
+  );
+
+  const startButton = welcomeScreen.find(`button`);
+
+  expect(startButton.length).toEqual(1);
+
+  startButton.simulate(`click`);
+
+  expect(clickHandler).toHaveBeenCalledTimes(1);
+});
