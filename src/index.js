ca99d64b87c930ef305b9fb773e4720a51685f06
Paul Petrov <paul@bargamut.ru>
2019-05-11 20:09:47 +0300

    Создать mock-данные и настройки

--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app.jsx';
+import settings, {questions} from './mocks/questions';
 
 const init = () => {
-  const settings = {
-    gameTime: 5,
-    errorCount: 3,
-    onClickStartBtn: () => {}
-  };
-
   ReactDOM.render(
       <App
         time={settings.gameTime}
---
+++ src/mocks/questions.js
@@ -0,0 +1,52 @@
+const settings = {
+  gameTime: 5,
+  errorCount: 3,
+  onClickStartBtn: () => {}
+};
+
+export const questions = [
+  {
+    type: `genre`,
+    title: `Выберите инди-рок треки`,
+    genre: `indie-rock`,
+    answers: [
+      {
+        genre: `indie-rock`,
+        src: `https://upload.wikimedia.org/wikipedia/commons/1/1f/Uganda_flag_and_national_anthem_-_Oh_Uganda_Land_o.ogg`
+      },
+      {
+        genre: `rock`,
+        src: `https://upload.wikimedia.org/wikipedia/commons/1/1f/Uganda_flag_and_national_anthem_-_Oh_Uganda_Land_o.ogg`
+      },
+      {
+        genre: `indie-rock`,
+        src: `https://upload.wikimedia.org/wikipedia/commons/1/1f/Uganda_flag_and_national_anthem_-_Oh_Uganda_Land_o.ogg`
+      },
+      {
+        genre: `folk-rock`,
+        src: `https://upload.wikimedia.org/wikipedia/commons/1/1f/Uganda_flag_and_national_anthem_-_Oh_Uganda_Land_o.ogg`
+      }
+    ]
+  },
+  {
+    type: `artist`,
+    title: `Кто исполняет эту песню?`,
+    artist: `Пелагея`,
+    answers: [
+      {
+        artist: `Пелагея`,
+        src: ``
+      },
+      {
+        artist: `Краснознаменная дивизия имени моей бабушки`,
+        src: ``
+      },
+      {
+        artist: `Lorde`,
+        src: ``
+      }
+    ]
+  }
+];
+
+export default settings;
