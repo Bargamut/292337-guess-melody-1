400085fde0825b1233120d149f27359cd2b91865
Paul Petrov <paul@bargamut.ru>
2019-05-12 01:07:15 +0300

    Сделать snapshot-тест для ArtistQuestionScreen

---
+++ src/components/artist-question-screen/artist-question-screen.test.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import ArtistQuestionScreen from './artist-question-screen.jsx';
+
+const mock = {
+  question: {
+    type: `artist`,
+    song: {
+      artist: `Пелагея`,
+      src: ``
+    },
+    answers: [
+      {
+        artist: `Пелагея`,
+        photo: ``
+      },
+      {
+        artist: `Краснознаменная дивизия имени моей бабушки`,
+        photo: ``
+      },
+      {
+        artist: `Lorde`,
+        photo: ``
+      }
+    ]
+  },
+  handleSubmit: jest.fn()
+};
+
+it(`Artist Question Screen renders correctly`, () => {
+  const {
+    question,
+    handleSubmit
+  } = mock;
+
+  const artistQuestionScreen = renderer.create(
+      <ArtistQuestionScreen
+        question={question}
+        onAnswer={handleSubmit}
+      />
+  ).toJSON();
+
+  expect(artistQuestionScreen).toMatchSnapshot();
+});
