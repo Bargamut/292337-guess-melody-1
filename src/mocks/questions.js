c311d0559ffe09ffd40d7d9b00fe5fc5e0e7abe6
Paul Petrov <paul@bargamut.ru>
2019-05-12 00:44:52 +0300

    Поправить структуру для ArtistQuestionScreen

--- src/mocks/questions.js
+++ src/mocks/questions.js
@@ -28,20 +28,22 @@ export const questions = [
   },
   {
     type: `artist`,
-    title: `Кто исполняет эту песню?`,
-    artist: `Пелагея`,
+    song: {
+      artist: `Пелагея`,
+      src: `https://upload.wikimedia.org/wikipedia/commons/1/1f/Uganda_flag_and_national_anthem_-_Oh_Uganda_Land_o.ogg`
+    },
     answers: [
       {
         artist: `Пелагея`,
-        src: ``
+        photo: `http://placehold.it/134x134`
       },
       {
         artist: `Краснознаменная дивизия имени моей бабушки`,
-        src: ``
+        photo: `http://placehold.it/134x134`
       },
       {
         artist: `Lorde`,
-        src: ``
+        photo: `http://placehold.it/134x134`
       }
     ]
   }
