14d7d5eb0fcd313efb8491feb85e78eefb9dab4c
Paul Petrov <paul@bargamut.ru>
2019-05-21 06:37:16 +0300

    Подключить хранилище и провайдер Redux

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+import {reducer} from './reducers/reducer';
+import Provider from 'react-redux';
 import App from './components/app/app.jsx';
 import settings, {questions} from './mocks/questions';
 
+const store = createStore(reducer);
+
 const init = (gameSettings, gameQuestions) => {
   ReactDOM.render(
-      <App
-        time={gameSettings.gameTime}
-        errorCount={gameSettings.errorCount}
-        questions={gameQuestions}
-      />,
+      <Provider store={store}>
+        <App
+          time={gameSettings.gameTime}
+          errorCount={gameSettings.errorCount}
+          questions={gameQuestions}
+        />
+      </Provider>,
       document.querySelector(`.main`)
   );
 };
