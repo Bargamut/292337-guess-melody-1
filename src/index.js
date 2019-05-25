9a8236fc1f1722a2dd270a552ab68b61b306f9a2
Paul Petrov <paul@bargamut.ru>
2019-05-25 03:18:32 +0300

    Fix import Provider

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {createStore} from 'redux';
 import {reducer} from './reducers/reducer';
-import Provider from 'react-redux';
+import {Provider} from 'react-redux';
 import App from './components/app/app.jsx';
 import settings, {questions} from './mocks/questions';
 
