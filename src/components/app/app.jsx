6798a9a0ba12837318f48567b897ae061311baa7
Paul Petrov <paul@bargamut.ru>
2019-06-12 14:08:23 +0300

    Импортировать Switch, Route в <App />

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import {Switch, Route} from 'react-router-dom';
 import {connect} from 'react-redux';
 
 import GameMistakes from '../game-mistakes/game-mistakes.jsx';
